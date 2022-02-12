883ce7931ef0691d221bf9f47468e5fe8a4e2548
Dict8-VIII <64312028+Dict8-VIII@users.noreply.github.com>
2022-02-12 23:26:52 +1100

    fix DHCP

--- _build/jupyter_execute/DHCP.py
+++ _build/jupyter_execute/DHCP.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# #DHCP 
+# # DHCP 
 # 
 # Client -> Discover -> Server <br>
 # Client <- Offer    <- Server <br>
 # Client -> Request  -> Server <br>
 # Client <- ACK      <- Server <br>
+
+# In[ ]:
+
+
+
+
