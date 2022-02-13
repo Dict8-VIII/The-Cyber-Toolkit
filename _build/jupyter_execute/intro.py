5d52c676f9c342c8c19c4f4acda4d6ae4be69836
Dict8-VIII <64312028+Dict8-VIII@users.noreply.github.com>
2022-02-13 21:14:20 +1100

    Better Intro, It's on Linked in now

--- _build/jupyter_execute/intro.py
+++ _build/jupyter_execute/intro.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 
 # # The Cyber Toolkit
-# I've written this book for a few reasons
+# Welcome to "The Cyber Toolkit", my private (and publicly shared) collection of Notes, Tools, Walkthroughs and general thoughts gathered from my Cyber Secuity study. Most content so far is from my own time on TryHackMe.com but will also include my notes during formal study. I've written this book for a few reasons.
 # 
 # - To keep track of and formalise my notes on my "Try Hack Me" experience
 # 
-# - To publish some write-ups when I feel confident enough in my knowledge
+# - To publish some write-ups when I feel confident enough in my knowledge (or enough to pretend)
 # 
 # - To show off my methods
 # 
@@ -15,8 +15,27 @@
 # - And simply because I can
 # 
 # 
-# As I progress through this I'll add extra sections and I'm sure modify things all over the place.
-#     
+# As I progress through this I'll add extra sections and I'm sure modify things all over the place. This is most certainly a work in progress; some sections are placeholders, others are incomplete (as I work through my older notes recording) and others are rather fleshed out. I'm still studying & learning, and this book will follow my own process so may not be the most logical for you.
+# 
+# That being said, I've designed this book to be a collection of tools which stand alone as much as possible. There is no need to start at the beginning or read everything I have on a topic before moving on; the search button is there for a reason. Promise I wont take it personally.
+# 
+# ### The Boring Stuff
+# I have written this book during my ETHICAL hacking study. Notice the key word there. If its not clear
+# 
+# > ETHICAL!!!!!
+# 
+# I cant stop you misusing any information you gather here, but my expectation of you (and a condition of access to this information) is that you will be the definition of a "White-Hat" hacker. No "little bit of grey hat". Bleach even. So white that Netflix has a restraining order...
+# 
+# 
+# ### A Bit about Me
+# 
+# My interest in Cyber Security started when my work purchased a PluralSight subscription and gave me an account. The work stuff wasnt very interesting, but I also found the Ethical Hacking Path run by Dale Meredith. It really captured my interest and I'd recommend it to anyone else who has a subscription (and also enjoys Dale's sense of humour). I only got to start Troy Hunt's one before my account ended though...
+# 
+# Since then, I have done my own little projects, but nothing too intensive. I tried HackTheBox.com, was interesting but didnt have enough background to take full advantage of it. Tried pwn.college, really wished I still had my notes from uni. TryHackMe I could really sink my teeth into though.
+# 
+# I should also take a minute to thank Emma. This book literally wouldnt exist without her. I learned python to help with her study (migrated from C) and she introduced me to Jupyter Notebook, lab & book; the later 2 are where this is being written. Thank you
+# 
+# Lastly, Abby, who is smiling on every page. She is far more stubborn than any room, tool, or application I've ever wrestled with. "Thanks".  She has shown me how dedicated I can be to a topic and how fulfilling it is to see them succeed, which is why she has been chosen as this book's logo. Besides, what dad doesnt like showing off his daughter.
 
 # In[ ]:
 
