f6f4033e75dad8b4380ed827ddeb9e0c9adb9e32
Dict8-VIII <64312028+Dict8-VIII@users.noreply.github.com>
2022-03-05 20:55:49 +1100

    finished upload vulns

--- _build/jupyter_execute/Craft/UploadVulns.py
+++ _build/jupyter_execute/Craft/UploadVulns.py
@@ -27,3 +27,44 @@
 # <hr>
 
 # ### Magic Numbers
+# 
+# If the previous section has left you thinking this is a bit all over the place; youre not alone. Wouldnt it be great if there was something in the file we could look at to determine what file type it is? Well thats exactly what 'Magic Numbers' are. These are some bits at the start of a file that anyone can inspect to determine what file type its SUPPOSED to be.... notice the supposed to be.... Some examples are
+# 
+# * .PNG      -> a .png file (0x 2E 50 4E 47)
+# * JPEG      -> a .jpeg file (0x 4A 50 45 47)
+# * \<?php      -> a .php file (0x 3C 3F 70 68 70)
+# 
+# >These are normally the first bits in the file (offset 0) but not always.
+# 
+# Heres why my PHP file looks like under a hex editor. On the left we can see the offset & raw values, on the right is the decoding of the file.
+# 
+# ![Craft-UploadVulns_phpHEX.png](../images/Craft-UploadVulns_phpHEX.png)
+# 
+# So, thats great. The file is defined by its contents, thats nice and secure, no issues of playing with the file name any more. Bugger, you've outsmarted me....
+# 
+# ![Craft-UploadVulns_outsmarted.png](../images/Craft-UploadVulns_outsmarted.png)
+# 
+# yea.... of course we have a way around this too. You see, while the magic number can be used for verifying a file, once the file is verified, the program just reads an executes the content. If we 'update' the magic number of a PHP file to be an image, a PHP server will still read the file and find its a .PHP file, and execute it. But we're getting ahead of ourselves. How do we update the magic numbers?
+# 
+# 1. Work out what we want our extension to be. In my case, I will turn a .PHP into a .PNG.
+# 
+# 2. As we are working with a PHP file, open it and insert 4 characters into the start of the file (ABCD in mine)
+# ![Craft-UploadVulns_Magic1.png](../images/Craft-UploadVulns_Magic1.png)
+# 
+# 3. Open the file with a Hexeditor (hexeditor comes pre-installed in Kali)
+# ![Craft-UploadVulns_Magic2.png](../images/Craft-UploadVulns_Magic2.png)
+# 
+# 4. Update the first 4 bytes (2x4 HEX chars) to the type we want. The updated values are GREEN
+# ![Craft-UploadVulns_Magic3.png](../images/Craft-UploadVulns_Magic3.png)
+# 
+# 5. Save the file and upload :-)
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
