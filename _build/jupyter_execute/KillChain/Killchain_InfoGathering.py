f7538d3466c4eba5a141d444c8740cb163369c95
Dict8-VIII <64312028+Dict8-VIII@users.noreply.github.com>
2022-03-28 23:51:39 +1100

    More sections

---
+++ _build/jupyter_execute/KillChain/Killchain_InfoGathering.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Killchain - Info Gathering
+# 
+# The first step in the Killchain process is Information Gathering. This centres around investigating a target using publicly available data. As a rule of thumb, if you're accessing the target then your in the next step already.<br>
+# So, what can we do without hitting the target? Well a lot actually.
+# 
+# 
---
+++ _build/jupyter_execute/Tools/Msfvenom.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Msfvenom
+# 
+# A component of Metasploit, this software can be used to create payloads for either meterpreter or other injection points.
+# 
+# 
